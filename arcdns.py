f5de9db1410fa161111a590139db9360321585b6
gene <gene@valimail.com>
2016-12-19 14:21:53 -0800

    test dns resolver working

---
+++ arcdns.py
@@ -0,0 +1,23 @@
+from dnslib.server import DNSServer, DNSLogger
+from dnslib.zoneresolver import ZoneResolver
+
+class ArcTestResolver(object):
+    def __init__(self, records, port=8053):
+        self.records = records
+        self.port = port
+
+    def __enter__(self):
+        zones = []
+        for (domain, txt) in self.records.items():
+            n = 255
+            frags = [("'" + txt[i:i+n] + "'") for i in range(0, len(txt), n)]
+            zones.append(domain + " IN TXT " + " ".join(frags))
+
+        resolver = ZoneResolver("\n".join(zones))
+
+        logger = DNSLogger()
+        self.server = DNSServer(resolver, port=self.port, logger=logger)
+        self.server.start_thread()
+
+    def __exit__(self, type, value, traceback):
+        self.server.stop()
--- testarc.py
+++ testarc.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.5
+#!/usr/bin/env python3
 
 # Run the ARC test suite in YAML format agains an implementation
 
@@ -6,68 +6,86 @@ import unittest
 import yaml
 import argparse
 
+import subprocess, os
+
 from ddt import ddt, data
 
-SIGN_TEST_FILE = "arc-draft-sign-tests.yml"
+from arcdns import ArcTestResolver
+
+SIGN_TEST_FILE   = "arc-draft-sign-tests.yml"
 VERIFY_TEST_FILE = "arc-draft-verify-tests.yml"
 
-def verify_test(script, test_case):
-    print(test_case)
+def verify_test(self, script, test_case, port=8053):
+    tmp_file = 'tmp/message.txt'
     print(test_case.tid)
 
-def sign_test(script, test_case):
+    with open(tmp_file,'w') as f:
+        f.write(test_case.test["message"])
+
+    with ArcTestResolver(test_case.txt_records, port):
+        proc = subprocess.Popen([script, tmp_file, str(port)], stdout=subprocess.PIPE)
+        out  = proc.communicate()[0].decode("utf-8")
+
+    self.assertEqual(out.lower(), test_case.test["cv"].lower())
+
+    os.remove(tmp_file)
+
+def sign_test(self, script, test_case, port=8080):
     print(test_case)
     print(test_case.tid)
 
 
 class ArcVerifyTestCase(object):
-    def __init__(self, tid, test, dkim_keys):
+    def __init__(self, tid, test, txt_records):
         self.tid = tid
         self.test = test
-        self.dkim_keys = dkim_keys
+        self.txt_records = txt_records
 
     def __str__(self):
         return ""
 
 class ArcSignTestCase(object):
-    def __init__(self, tid, test, domain, sel, headers, private, dkim_keys):
+    def __init__(self, tid, test, domain, sel, headers, private, txt_records):
         self.tid = tid
         self.test = test
         self.domain = domain
         self.sel = sel
         self.headers = headers
         self.private = private
-        self.dkim_keys = dkim_keys
+        self.txt_records = txt_records
 
     def __str__(self):
         return ""
 
-def main(op, script, verbose=False):
+def main(op, script, test=None, verbose=False):
     tests = []
 
     if op == "verify":
         scenarios = list(yaml.safe_load_all(open(VERIFY_TEST_FILE, 'rb')))
         for scenario in scenarios:
-            tests += [ArcVerifyTestCase(k, v, scenario["dkim_keys"]) for
+            tests += [ArcVerifyTestCase(k, v, scenario["txt_records"]) for
                       (k, v) in scenario["tests"].items()]
     elif op == "sign":
         scenarios = list(yaml.safe_load_all(open(SIGN_TEST_FILE, 'rb')))
         for scenario in scenarios:
             tests += [ArcSignTestCase(k, v, scenario["domain"], scenario["sel"], scenario["headers"],
-                                      scenario["private"], scenario["dkim_keys"]) for
+                                      scenario["private"], scenario["txt_records"]) for
                       (k, v) in scenario["tests"].items()]
     else:
         raise ValueError("invalid operation")
 
-    # this is a little odd
+    # use test variable here
+
+    # This is a little odd, but it lets us dynamically
+    # create tests from the test file
     @ddt
     class ArcTest(unittest.TestCase):
         @data(*tests)
         def test(self, test_case):
             if op == "verify":
-                verify_test(script, test_case)
+                verify_test(self, script, test_case)
             else:
-                sign_test(script, test_case)
+                sign_test(self, script, test_case)
 
     suite = unittest.TestLoader().loadTestsFromTestCase(ArcTest)
     v = 2 if verbose else 1
@@ -76,10 +94,9 @@ def main(op, script, verbose=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the ARC test suite in YAML format against an implementation.')
     parser.add_argument('op', choices=["sign", "verify"], default="sign", help='Operation to test')
-    parser.add_argument('script', help='A command line implementation of an arc signing or verification routing. ')
+    parser.add_argument('script', help='A command line implementation of an arc signing or verification routine. The arguments to the signing script must be [messagefile dnsport selector domain privatekeyfile headers], the arguments to the verification script must be [messagefile dnsport].')
     parser.add_argument('-t', dest='test', metavar='TEST', required=False, help='Specific test to run')
     parser.add_argument('-v', dest='verbose', action='store_true', required=False, help='verbose')
 
     args = parser.parse_args()
-
-    main(args.op, args.script, args.verbose)
+    main(args.op, args.script, args.test, args.verbose)
